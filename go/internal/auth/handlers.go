67f8b94b1f836c800c7aa9dc6e8902f7f78c4bf1
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-05-08 18:32:38 +0300

    Added the creating user handler

--- go/internal/auth/handlers.go
+++ go/internal/auth/handlers.go
@@ -1,11 +1,41 @@
 package auth
 
 import (
+	"context"
+	"cu_coworking_book/go/internal/db"
 	"cu_coworking_book/go/internal/pb"
 	"sync"
+
+	"golang.org/x/crypto/bcrypt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type AuthServer struct {
 	pb.UnimplementedAuthenticationServer
 	mu sync.RWMutex
+}
+
+func (s *AuthServer) CreateUser(ctx context.Context, req *pb.User) (*pb.UserId, error) {
+
+	hashPassword, err := bcrypt.GenerateFromPassword([]byte(req.GetPswd()), 12)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "create user error: generate hash password: %v", err)
+	}
+	user := db.User{
+		Email:      req.GetEmail(),
+		FirstName:  req.GetFirstName(),
+		SecondName: req.GetSecondName(),
+		MidName:    req.GetMidName(),
+		Password:   string(hashPassword),
+	}
+
+	s.mu.Lock()
+	id, err := db.CreateUser(user)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+	s.mu.Unlock()
+
+	return &pb.UserId{Id: id}, nil
 }
