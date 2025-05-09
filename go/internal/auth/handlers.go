3b42ce7645a39084ccd52a8b1495e3ad53510077
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-05-09 09:32:02 +0300

    Added log in handler

--- go/internal/auth/handlers.go
+++ go/internal/auth/handlers.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"cu_coworking_book/go/internal/db"
 	"cu_coworking_book/go/internal/pb"
+	"database/sql"
 	"encoding/base64"
+	"errors"
 	"sync"
 
 	"golang.org/x/crypto/bcrypt"
@@ -19,32 +21,49 @@ type AuthServer struct {
 
 func (s *AuthServer) SignUpUser(ctx context.Context, req *pb.SignUpRequest) (*pb.SignUpResponse, error) {
 
-	var result *pb.SignUpResponse
-
 	password, err := bcrypt.GenerateFromPassword([]byte(req.GetPassword()), 14)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "sign up error: generate password hash: %v", err)
 	}
 
 	user := db.User{
-		Email:          req.GetEmail(),
-		FirstName:      req.GetFirstName(),
-		SecondName:     req.GetSecondName(),
-		Patronymic:     req.GetPatronymic(),
-		HashedPassword: string(password),
+		Email:      req.GetEmail(),
+		FirstName:  req.GetFirstName(),
+		SecondName: req.GetSecondName(),
+		Patronymic: req.GetPatronymic(),
+		Password:   string(password),
 	}
 
 	s.mu.Lock()
 	res, err := db.CreateUser(user)
 	s.mu.Unlock()
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "sign up error: generate password hash: %v", err)
+		return nil, status.Errorf(codes.Internal, "sign up error: db error: %v", err)
 	}
-	result.Id = res.ID
-	result.Role = res.Role
 
 	emailPassword := req.GetEmail() + ":" + req.GetPassword()
-	result.AuthHeader = base64.StdEncoding.EncodeToString([]byte(emailPassword))
+	authHeader := base64.StdEncoding.EncodeToString([]byte(emailPassword))
 
-	return result, nil
+	return &pb.SignUpResponse{Id: res.ID, Role: res.Role, AuthHeader: authHeader}, nil
+}
+
+func (s *AuthServer) LogInUser(ctx context.Context, req *pb.EmailPassword) (*pb.AuthHeader, error) {
+
+	s.mu.RLock()
+	expectedLogin, err := db.GetUserByEmail(req.GetEmail())
+	s.mu.RUnlock()
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, status.Errorf(codes.NotFound, "log in error: db error: %v", err)
+		} else {
+			return nil, status.Errorf(codes.Internal, "log in error: db error: %v", err)
+		}
+	}
+
+	if expectedLogin.Password == req.GetPassword() {
+		authHeader := base64.StdEncoding.EncodeToString([]byte(req.GetEmail() + ":" + req.GetPassword()))
+		return &pb.AuthHeader{AuthHeader: authHeader}, nil
+	} else {
+		return nil, status.Error(codes.InvalidArgument, "log in error: invalid password")
+	}
 }
