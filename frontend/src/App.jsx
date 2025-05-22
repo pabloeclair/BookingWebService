408eaf8ca150ad5f614e824f362e7c1c0097f711
Pablo_3_16 <ergle.pasha@gmail.com>
2025-05-22 13:32:29 +0300

    Implemented the draft of authorisation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import './App.css'
+import AuthProvider from "./auth/AuthProvider.jsx";
+import {BrowserRouter, Route, Routes} from "react-router";
+import Signup from "./auth/Signup.jsx";
+import React from "react";
+import Login from "./auth/Login.jsx";
+
 function App() {
     return (
-        <></>
-  )
+        <AuthProvider>
+            <BrowserRouter>
+                <img src={"../resources/Central_University_full.png"}
+                     id={"logo"} alt={"Логотип Центрального университета"} key={"cu_logo"}/>
+                <Routes>
+                    <Route path={"/user/login"} element={<Login/>}/>
+                    <Route path={"user/signup"} element={<Signup/>}/>
+                </Routes>
+            </BrowserRouter>
+        </AuthProvider>
+    );
 }
 
 export default App
