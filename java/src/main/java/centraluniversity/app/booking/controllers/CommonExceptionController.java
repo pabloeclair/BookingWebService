c7a244f60aecb18ceeaa692aa22eb1554d3182e0
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-05-25 22:32:32 +0300

    Implemented Auth Controller, Model & Service

---
+++ java/src/main/java/centraluniversity/app/booking/controllers/CommonExceptionController.java
@@ -0,0 +1,31 @@
+package centraluniversity.app.booking.controllers;
+
+import centraluniversity.app.booking.models.exception.ErrorDto;
+import centraluniversity.app.booking.models.exception.HttpStatusException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CommonExceptionController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(e.getStatusCode())
+                .body(new ErrorDto(e.getStatusCode().toString(), e.getMessage()));
+    }
+
+    @ExceptionHandler(HttpStatusException.class)
+    public ResponseEntity<ErrorDto> handleHttpStatusException(HttpStatusException e) {
+        return ResponseEntity.status(e.getStatus())
+                .body(new ErrorDto(e.getStatus().toString(), e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorDto("500 INTERNAL_SERVER_ERROR", e.getMessage()));
+    }
+}
---
+++ java/src/main/java/centraluniversity/app/booking/models/auth/UserResponseDto.java
@@ -0,0 +1,20 @@
+package centraluniversity.app.booking.models.auth;
+
+import centraluniversity.app.booking.pb.Role;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+    
+    private int id;
+    private String email;
+    private String firstName;
+    private String secondName;
+    private String patronymic;
+    private String token;
+    private Role role;
+    
+}
