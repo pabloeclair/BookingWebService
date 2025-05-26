f3b5c983324d0351a3cc61f31d88460ae12cf272
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-05-26 15:21:46 +0300

    Added frontend docker file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        host: true,
+        port: 3000,
+    },
+    preview: {
+        host: true,
+        port: 3000,
+    },
+});
+
