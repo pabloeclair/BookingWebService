f87f59f711ca1c1127bfd8a93bb6b77b7b40f36b
Pablo_3_16 <ergle.pasha@gmail.com>
2025-05-26 15:04:59 +0300

    Changed pathes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import AuthProvider from "./auth/AuthProvider.jsx";
 import {BrowserRouter, Route, Routes} from "react-router";
 import Signup from "./auth/Signup.jsx";
 import React from "react";
-import Login from "./auth/Login.jsx";
+import LoginPage from "./auth/LoginPage.jsx";
 import Navigation from "./Navigation.jsx"
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
                 <img src={"../resources/Central_University_full.png"}
                      id={"logo"} alt={"Логотип Центрального университета"} key={"cu_logo"}/>
                 <Routes>
-                    <Route path={"/user/login"} element={<Login/>}/>
-                    <Route path={"/user/signup"} element={<Signup/>}/>
+                    <Route path={"/login"} element={<LoginPage/>}/>
+                    <Route path={"/signup"} element={<Signup/>}/>
                     <Route path={"*"} element={<Navigation/>}/>
                 </Routes>
             </AuthProvider>
