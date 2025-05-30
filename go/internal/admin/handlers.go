f325d14bda386d82b5264181da38b285dc3be9da
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-05-30 20:15:06 +0300

    Added creating user

--- go/internal/admin/handlers.go
+++ go/internal/admin/handlers.go
@@ -2,11 +2,17 @@ package admin
 
 import (
 	"context"
+	"cu_coworking_book/go/internal/db"
 	"cu_coworking_book/go/internal/pb"
+	"cu_coworking_book/go/internal/utils"
+	"errors"
 	"log"
 	"sync"
 
+	"golang.org/x/crypto/bcrypt"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type AdminService struct {
@@ -24,4 +30,41 @@ func LogInterceptor(ctx context.Context, req interface{}, info *grpc.UnaryServer
 	}
 
 	return resp, err
+}
+
+func (s *AdminService) CreateUser(ctx context.Context, req *pb.CreateRequestAdmin) (*pb.UserResponse, error) {
+
+	s.mu.RLock()
+	_, err := utils.ComparePassword(req.AdminEmail, []byte(req.AdminPassword), true)
+	s.mu.RUnlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), 12)
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	user := db.User{
+		Email:      req.GetEmail(),
+		FirstName:  req.GetFirstName(),
+		SecondName: req.GetSecondName(),
+		Patronymic: req.GetPatronymic(),
+		Password:   string(hashedPassword),
+	}
+
+	s.mu.Lock()
+	res, err := db.CreateUser(user)
+	s.mu.Unlock()
+
+	if err != nil {
+		if errors.Is(err, db.ErrBadRequest) {
+			return nil, status.Error(codes.AlreadyExists, err.Error())
+		}
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return utils.ParseToResult(res), nil
 }
