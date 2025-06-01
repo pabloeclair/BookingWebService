95b79c0cd5fbd0502b0f3b29ec99283483879491
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-02 00:43:19 +0300

    Rename AdminController & changed input

---
+++ java/src/main/java/centraluniversity/app/booking/controllers/AdminUserController.java
@@ -0,0 +1,46 @@
+package centraluniversity.app.booking.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.*;
+
+import centraluniversity.app.booking.models.user.CreateUserDto;
+import centraluniversity.app.booking.models.user.GetUserDto;
+import centraluniversity.app.booking.models.user.UpdateUserDto;
+import centraluniversity.app.booking.models.user.UserResponseDto;
+import centraluniversity.app.booking.pb.By;
+import centraluniversity.app.booking.services.AdminUserService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/admin/users")
+@RequiredArgsConstructor
+public class AdminUserController {
+    
+    private final AdminUserService adminService;
+
+    @PostMapping()
+    public UserResponseDto createUser(@Valid @RequestBody CreateUserDto user) {
+        return adminService.createUser(user);
+    }
+
+    @GetMapping("/{sort_key}")
+    public GetUserDto getUser(@PathVariable("sort_key") String sortKey,
+        @RequestParam(name = "email", required = true) String email,
+        @RequestParam(name = "key", required = true) String key,
+        @RequestParam(name = "by", required = true) String sortBy) {
+        return adminService.getUser(email, key, By.valueOf(sortBy), sortKey);
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDto updateUser(@PathVariable("id") Integer id, @RequestBody @Valid UpdateUserDto user) {
+        return adminService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Integer id,
+        @RequestParam(name = "email", required = true) String email,
+        @RequestParam(name = "key", required = true) String key) {
+        adminService.deleteUser(id, email, key);
+    }
+}
--- java/src/main/java/centraluniversity/app/booking/controllers/AuthController.java
+++ java/src/main/java/centraluniversity/app/booking/controllers/AuthController.java
@@ -2,15 +2,10 @@ package centraluniversity.app.booking.controllers;
 
 import javax.validation.Valid;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import centraluniversity.app.booking.models.UserResponseDto;
-import centraluniversity.app.booking.models.auth.SignupUserDto;
+import centraluniversity.app.booking.models.user.SignupUserDto;
+import centraluniversity.app.booking.models.user.UserResponseDto;
 import centraluniversity.app.booking.services.AuthService;
 import lombok.RequiredArgsConstructor;
 
@@ -28,7 +23,9 @@ public class AuthController {
     }   
 
     @GetMapping()
-    public UserResponseDto getUserByEmail(@PathVariable String email, @PathVariable String key) throws Exception {
+    public UserResponseDto login(
+        @RequestParam(name = "email", required = true) String email,
+        @RequestParam(name = "key", required = true) String key) throws Exception {
         return authService.getUserByEmail(email, key);
     }
 
