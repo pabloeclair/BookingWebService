2839dc117f989958045eb272fbb0dd57c9e839e0
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-06 20:31:24 +0300

    Added tests for validation of time

---
+++ java/src/test/java/centraluniversity/app/booking/services/BookingServiceTest.java
@@ -0,0 +1,121 @@
+package centraluniversity.app.booking.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import centraluniversity.app.booking.models.booking.TimeBookingDto;
+import centraluniversity.app.booking.models.exception.HttpStatusException;
+
+public class BookingServiceTest {
+
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    
+    // validateDateTime tests
+
+    @Test
+    public void testValidateDateTime_CorrectRequest() {
+
+        // Array
+        LocalDate bookingDate = LocalDate.parse("2044-04-04", dateFormatter);
+        LocalTime bookingStart = LocalTime.of(8, 20);
+        LocalTime bookingEnd = LocalTime.of(10, 20);
+
+        // Action & Assert
+        BookingService.validateDateTime(bookingDate, bookingStart, bookingEnd);
+    }
+
+    @Test
+    public void testValidateDateTime_IllegalRequest_IllegalBookingDate() {
+
+        // Array
+        LocalDate bookingDate = LocalDate.parse("2023-04-04", dateFormatter);
+        LocalTime bookingStart = LocalTime.of(8, 20);
+        LocalTime bookingEnd = LocalTime.of(10, 20);
+        String expectedError = "Нельзя забронировать аудиторию ранее сегодняшнего дня";
+
+        // Action & Assert
+        HttpStatusException e = assertThrows(HttpStatusException.class,  
+            () -> BookingService.validateDateTime(bookingDate, bookingStart, bookingEnd));
+        assertEquals(HttpStatus.BAD_REQUEST, e.getStatus());
+        assertEquals(expectedError, e.getMessage());
+    }
+
+    @Test
+    public void testValidateDateTime_IllegalRequest_IllegalBookingStart() {
+
+        // Array
+        LocalDate bookingDate = LocalDate.parse("2044-04-04", dateFormatter);
+        LocalTime bookingStart = LocalTime.of(12, 20);
+        LocalTime bookingEnd = LocalTime.of(10, 20);
+        String expectedError = "Время начала должно быть строго раньше времени окончания";
+
+        // Action & Assert
+        HttpStatusException e = assertThrows(HttpStatusException.class,  
+            () -> BookingService.validateDateTime(bookingDate, bookingStart, bookingEnd));
+        assertEquals(HttpStatus.BAD_REQUEST, e.getStatus());
+        assertEquals(expectedError, e.getMessage());
+    }
+
+    @Test
+    public void testValidateDateTime_IllegalRequest_BookingTimesEquals() {
+
+        // Array
+        LocalDate bookingDate = LocalDate.parse("2044-04-04", dateFormatter);
+        LocalTime bookingStart = LocalTime.of(12, 20);
+        LocalTime bookingEnd = LocalTime.of(12, 20);
+        String expectedError = "Время начала должно быть строго раньше времени окончания";
+
+        // Action & Assert
+        HttpStatusException e = assertThrows(HttpStatusException.class,  
+            () -> BookingService.validateDateTime(bookingDate, bookingStart, bookingEnd));
+        assertEquals(HttpStatus.BAD_REQUEST, e.getStatus());
+        assertEquals(expectedError, e.getMessage());
+    }
+
+    // validateTimesByOneDay tests
+
+    private static List<TimeBookingDto> generateSampleTimeBookings() {
+        List<TimeBookingDto> bookings = new ArrayList<>();
+
+        TimeBookingDto booking2 = new TimeBookingDto();
+        booking2.setBookingStart(LocalTime.of(9, 30));  
+        booking2.setBookingEnd(LocalTime.of(10, 30));   
+        bookings.add(booking2);
+
+        TimeBookingDto booking3 = new TimeBookingDto();
+        booking3.setBookingStart(LocalTime.of(11, 0));  
+        booking3.setBookingEnd(LocalTime.of(12, 0));    
+        bookings.add(booking3);
+
+        TimeBookingDto booking4 = new TimeBookingDto();
+        booking4.setBookingStart(LocalTime.of(14, 0));  
+        booking4.setBookingEnd(LocalTime.of(15, 0));    
+        bookings.add(booking4);
+
+        return bookings;
+    }
+
+    @Test
+    public void testValidateTimesByOneDay_CorrectRequest_WithArray() {
+
+        // Array
+        List<TimeBookingDto> array = generateSampleTimeBookings();
+        LocalTime bookingStart = LocalTime.of(16, 20);
+        LocalTime bookingEnd = LocalTime.of(18, 20);
+
+        // Action & Assert
+        BookingService.validateTimesByOneDay(array, bookingStart, bookingEnd);
+    }
+
+    
+    
+}
