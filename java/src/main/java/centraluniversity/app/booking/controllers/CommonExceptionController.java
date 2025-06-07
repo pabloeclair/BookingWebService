2cdcc544e8e6839fbaf04ca9c18b992957395a19
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-07 13:57:48 +0300

    added error type

--- java/src/main/java/centraluniversity/app/booking/controllers/CommonExceptionController.java
+++ java/src/main/java/centraluniversity/app/booking/controllers/CommonExceptionController.java
@@ -14,18 +14,18 @@ public class CommonExceptionController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorDto> methodArgumentNotValidException(MethodArgumentNotValidException e) {
         return ResponseEntity.status(e.getStatusCode())
-                .body(new ErrorDto(e.getStatusCode().toString(), e.getMessage()));
+                .body(new ErrorDto(e.getStatusCode().toString(), null, e.getMessage()));
     }
 
     @ExceptionHandler(HttpStatusException.class)
     public ResponseEntity<ErrorDto> handleHttpStatusException(HttpStatusException e) {
         return ResponseEntity.status(e.getStatus())
-                .body(new ErrorDto(e.getStatus().toString(), e.getMessage()));
+                .body(new ErrorDto(e.getStatus().toString(), null, e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handleException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorDto("500 INTERNAL_SERVER_ERROR", e.getMessage()));
+                .body(new ErrorDto("500 INTERNAL_SERVER_ERROR", e.getClass().getName(), e.getMessage()));
     }
 }
