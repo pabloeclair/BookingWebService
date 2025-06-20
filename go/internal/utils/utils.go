41ee18409217b1a0d9f8ee20d6d4773c2f302a3d
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-20 16:23:20 +0300

    fully updated file

--- go/internal/utils/utils.go
+++ go/internal/utils/utils.go
@@ -1,57 +1,30 @@
 package utils
 
 import (
-	"context"
-	"cu_coworking_book/go/internal/db"
-	"cu_coworking_book/go/internal/pb"
-	"errors"
-
-	"golang.org/x/crypto/bcrypt"
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+	"log"
+	"strconv"
 )
 
-// Сверяет тип ошибки и возвращает NotFound или Internal.
-func CompareErrAndErrNotFound(err error) error {
-	if errors.Is(err, db.ErrNotFound) {
-		return status.Error(codes.NotFound, err.Error())
+// Проверяет наличие и корректность переменных окружения, указывающих продолжительность
+// JWT-токенов и в случае успеха возвращает указанное число или число по умолчанию (60) при отсутствии
+func CheckJWTDuration(duration string, isAdmin bool) int {
+	var user string
+	if isAdmin {
+		user = "ADMIN"
 	} else {
-		return status.Error(codes.Internal, err.Error())
-	}
-}
-
-// Сверяет пароль указанного пользователя, а также проверяет роль при выполнении
-// команд администратора.
-func ComparePassword(ctx context.Context, email string, password string, admin bool) (db.User, error) {
-	user, err := db.GetUserByEmail(ctx, email)
-
-	if err != nil {
-		return user, CompareErrAndErrNotFound(err)
+		user = "USER"
 	}
 
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	adminPermission := admin && (user.Role != pb.Role_ADMIN.String() && user.Role != pb.Role_MAIN_ADMIN.String())
-
-	if err != nil || adminPermission {
-		return user, status.Error(codes.PermissionDenied, "доступ запрещен")
-	}
-	return user, nil
-}
-
-// Преобразовывает db.User в pb.GetResponse, а также возвращает ФИО с Большой Буквы.
-func ParseToResult(res db.User) *pb.GetResponse {
-
-	c := cases.Title(language.Russian)
-
-	return &pb.GetResponse{
-		Id:         res.ID,
-		Email:      res.Email,
-		FirstName:  c.String(res.FirstName),
-		SecondName: c.String(res.SecondName),
-		Patronymic: c.String(res.Patronymic),
-		Password:   res.Password,
-		Role:       pb.Role(pb.Role_value[res.Role]),
+	if duration == "" {
+		log.Println("ПРЕДУПРЕЖДЕНИЕ: в переменной окружения JWT_" + user + "_DURATION, которая указывает на " +
+			"продолжительность в минутах сохранения JWT-токенов, было пропущено значение, в следствии чего будет " +
+			"использоваться значение по умолчанию - 60 минут")
+		return 60
+	} else {
+		durationInt, err := strconv.Atoi(duration)
+		if err != nil {
+			log.Fatalf("ошибка окружения: необходимо указывать значение JWT_" + user + "_DURATION в формате int (в минутах)")
+		}
+		return durationInt
 	}
 }
