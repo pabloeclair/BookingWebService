64b96626376c44ea58504ae84afd617b3535c7cc
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-23 19:52:35 +0300

    Added comments & small fixes

--- go/cmd/server/server.go
+++ go/cmd/server/server.go
@@ -23,13 +23,12 @@ func main() {
 	}
 	addrs := os.Args[1]
 
+	// проверка перменных окружения
 	godotenv.Load()
-
 	secretKey := os.Getenv("JWT_SECRET_KEY")
 	if secretKey == "" {
 		log.Fatal("ошибка запуска: необходимо указать значение окружения JWT_SECRET_KEY для генерации JWT-токенов")
 	}
-
 	durationUser := os.Getenv("JWT_USER_DURATION")
 	utils.CheckEnvJWTDuration(durationUser, false)
 	durationAdmin := os.Getenv("JWT_ADMIN_DURATION")
@@ -39,13 +38,19 @@ func main() {
 	defer cancel()
 
 	mux := http.NewServeMux()
+	// auth
 	mux.HandleFunc("/api/v1/signup", handlers.SignupUser)
 	mux.HandleFunc("/api/v1/login", handlers.LoginUser)
 
+	// personal account
 	mux.HandleFunc("PUT /api/v1/user", handlers.UpdateUser)
 	mux.HandleFunc("DELETE /api/v1/user", handlers.DeleteUser)
 	mux.HandleFunc("/api/v1/user/password", handlers.UpdatePassword)
 
+	// admin
+	// todo: admin handlers
+
+	// exseptions
 	mux.HandleFunc("/api/v1/user", handlers.MethodNotAllowedException)
 	mux.HandleFunc("/", handlers.NotFoundException)
 
@@ -67,12 +72,17 @@ func main() {
 		return
 	}
 
+	// создание таблицы, если не существует
 	if err := db.CreateTable(); err != nil {
+		if err := s.Shutdown(context.Background()); err != nil {
+			log.Printf("ошибка закрытия сервера: %s", err)
+		}
 		log.Fatalf("ошибка подключения к бд: %s", err.Error())
 	}
 
 	log.Println("сервер запущен")
 
+	// graseful shutdown
 	<-ctx.Done()
 	log.Println("сервер закрывается...")
 	<-time.After(time.Second * 3)
