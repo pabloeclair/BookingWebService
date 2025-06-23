c2117c8df1d7d25c1cca7569452411a05bdaf9db
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-23 23:22:26 +0300

    deleted protobuf

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.6"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("com.google.protobuf") version "0.9.5"
 }
 
 group = "centraluniversity.app"
@@ -31,11 +30,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.postgresql:postgresql:42.7.2")
 	implementation("javax.validation:validation-api:2.0.1.Final")
-	
-	implementation("com.google.protobuf:protobuf-java:4.30.2")
-    implementation("io.grpc:grpc-netty-shaded:1.51.0")
-    implementation("io.grpc:grpc-protobuf:1.51.0")
-    implementation("io.grpc:grpc-stub:1.51.0")
 	implementation("javax.annotation:javax.annotation-api:1.3.2")
 
 	compileOnly("org.projectlombok:lombok")
@@ -47,24 +41,6 @@ dependencies {
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-protobuf {
-	protoc {
-        artifact = "com.google.protobuf:protoc:3.21.12"  
-    }
-    plugins {
-        create("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.51.0"
-        }
-    }
-    generateProtoTasks {
-        all().forEach {
-            it.plugins {
-                create("grpc")
-            }
-        }
-    }
 }
 
 tasks.withType<Test> {
