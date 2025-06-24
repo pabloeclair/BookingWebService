5dae821a959e19ed7b91dd81b8253afae5ff5620
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-24 22:52:51 +0300

    Implemented booking admin controller

--- java/src/main/java/centraluniversity/app/booking/controllers/AdminBookingController.java
+++ java/src/main/java/centraluniversity/app/booking/controllers/AdminBookingController.java
@@ -1,56 +1,76 @@
-// package centraluniversity.app.booking.controllers;
+package centraluniversity.app.booking.controllers;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.http.HttpStatus;
-// import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-// import centraluniversity.app.booking.models.booking.BookingDto;
-// import centraluniversity.app.booking.models.booking.CreateBookingDto;
-// import centraluniversity.app.booking.models.booking.UpdateBookingDto;
-// import centraluniversity.app.booking.services.BookingService;
-// import io.swagger.v3.oas.annotations.Operation;
-// import io.swagger.v3.oas.annotations.parameters.RequestBody;
-// import io.swagger.v3.oas.annotations.tags.Tag;
-// import jakarta.validation.Valid;
-// import lombok.RequiredArgsConstructor;
+import centraluniversity.app.booking.models.booking.BookingCreateDto;
+import centraluniversity.app.booking.models.booking.BookingDbDto;
+import centraluniversity.app.booking.models.exception.HttpStatusException;
+import centraluniversity.app.booking.models.user.Role;
+import centraluniversity.app.booking.models.user.UserDto;
+import centraluniversity.app.booking.services.AuthService;
+import centraluniversity.app.booking.services.BookingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
-// @Tag(name = "AdminBooking API", description = "Административное управление бронями")
-// @RestController
-// @RequestMapping("/admin/booking")
-// @RequiredArgsConstructor
-// public class AdminBookingController {
+@Tag(name = "AdminBooking API", description = "Административное управление бронями")
+@RestController
+@RequestMapping("/api/v1/admin/booking")
+@RequiredArgsConstructor
+public class AdminBookingController {
 
-//     private final BookingService bookingService;
+    private final BookingService bookingService;
+    private final AuthService authService;
     
-//     @Operation(summary = "Создание новой брони")
-//     @PostMapping
-//     @ResponseStatus(HttpStatus.CREATED)
-//     public void createBooking(@RequestBody @Valid CreateBookingDto bookingDto) throws Exception {
-//         bookingService.createBooking(bookingDto, true);
-//     }
+    @Operation(summary = "Создание новой брони")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createBooking(
+            @RequestBody @Valid BookingCreateDto bookingDto,
+            @RequestHeader("Authorization") String tokenString
+    ) {
+        auth(tokenString);
+        bookingService.createBooking(bookingDto.parseToDb());
+    }
 
-//     @Operation(summary = "Получение всех доступных броней")
-//     @GetMapping
-//     public List<BookingDto> getAllBookings(
-//             @RequestParam(name = "email", required = true) String email,
-//             @RequestParam(name = "key", required = true) String key) throws Exception {
-//         return bookingService.getAllBookings(email, key);
-//     }
+    @Operation(summary = "Получение всех доступных броней")
+    @GetMapping
+    public List<BookingDbDto> getAllBookings(@RequestHeader("Authorization") String tokenString) {
+        auth(tokenString);
+        return bookingService.getAllBookings();
+    }
 
-//     @Operation(summary = "Обновление информации о брони")
-//     @PutMapping("/{id}")
-//     @ResponseStatus(HttpStatus.NO_CONTENT)
-//     public void updateBooking(@PathVariable("id") Integer id, @RequestBody @Valid UpdateBookingDto booking) throws Exception {
-//         bookingService.updateBooking(id, booking, true);
-//     }
+    @Operation(summary = "Обновление информации о брони")
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateBooking(
+            @PathVariable("id") Integer id, 
+            @RequestBody @Valid BookingDbDto booking,
+            @RequestHeader("Authorization") String tokenString
+    ) {
+        auth(tokenString);
+        bookingService.updateBooking(id, booking);
+    }
 
-//     @Operation(summary = "Отмена брони")
-//     @DeleteMapping("/{id}")
-//     @ResponseStatus(HttpStatus.NO_CONTENT)
-//     public void deleteBooking(@PathVariable("id") Integer id,
-//             @RequestParam(name = "email", required = true) String email,
-//             @RequestParam(name = "key", required = true) String key) throws Exception {
-//         bookingService.deleteBooking(id, email, key, true);
-//     }
-// }
+    @Operation(summary = "Отмена брони")
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBooking(
+            @PathVariable("id") Integer id,
+            @RequestHeader("Authorization") String tokenString
+    ) {
+        auth(tokenString);
+        bookingService.deleteBooking(id);
+    }
+
+    private void auth(String tokenString) throws HttpStatusException {
+        UserDto user = authService.parseJwt(tokenString);
+        if (!user.getRole().equals(Role.ADMIN) && !user.getRole().equals(Role.MAIN_ADMIN)) {
+            throw new HttpStatusException(HttpStatus.FORBIDDEN, "доступ запрещен");
+        }
+    }
+}
