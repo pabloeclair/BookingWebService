ff4249be6400433ba18f9c7f065d3bcec6d25fdc
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-24 14:59:34 +0300

    Added new handlers and comments

--- java/src/main/java/centraluniversity/app/booking/controllers/CommonExceptionController.java
+++ java/src/main/java/centraluniversity/app/booking/controllers/CommonExceptionController.java
@@ -2,37 +2,59 @@ package centraluniversity.app.booking.controllers;
 
 import centraluniversity.app.booking.models.exception.ErrorDto;
 import centraluniversity.app.booking.models.exception.HttpStatusException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class CommonExceptionController {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorDto> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        return ResponseEntity.status(e.getStatusCode())
-                .body(new ErrorDto(e.getStatusCode().toString(), e.getMessage()));
-    }
-
+    // Обработчик HttpStatusException
     @ExceptionHandler(HttpStatusException.class)
     public ResponseEntity<ErrorDto> handleHttpStatusException(HttpStatusException e) {
         return ResponseEntity.status(e.getStatus())
                 .body(new ErrorDto(e.getStatus().toString(), e.getMessage()));
     }
 
+    // Обработчик отсутствия заголовка
     @ExceptionHandler(MissingRequestHeaderException.class)
-    public ResponseEntity<ErrorDto> handleHttpStatusException(MissingRequestHeaderException e) {
+    public ResponseEntity<ErrorDto> handleMissingRequestHeaderException(MissingRequestHeaderException e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorDto("400 BAD REQUEST", e.getMessage()));
+                .body(new ErrorDto("400 BAD_REQUEST", e.getMessage()));
     }
 
+    // Обработчик не пройденной валидации
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        List<String> errorFields = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        String errorMessage = String.join(", ", errorFields);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorDto("400 BAD_REQUEST", errorMessage));
+    }
+
+    // Обработчик не найденного адресного пути
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorDto> handleNoResourceFoundException(NoResourceFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorDto("404 NOT_FOUND", e.getMessage()));
+    }
+
+    // Обработчик остальных ошибок
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handleException(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorDto("500 INTERNAL_SERVER_ERROR", e.getClass().getName() + ":" + e.getMessage()));
+                .body(new ErrorDto("500 INTERNAL_SERVER_ERROR", e.getClass().getName() + ": " + e.getMessage()));
     }
 }
