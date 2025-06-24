3f2119093a0b9f51ea82d31f735fe43e054ff3d9
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-24 14:58:31 +0300

    added no args contructor

--- java/src/main/java/centraluniversity/app/booking/models/exception/ErrorDto.java
+++ java/src/main/java/centraluniversity/app/booking/models/exception/ErrorDto.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ErrorDto {
 
     @JsonProperty("status_code")
