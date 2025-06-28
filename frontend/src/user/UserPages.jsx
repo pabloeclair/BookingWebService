0d0f42e7f29aceab1ab8cb83004539182467e7dd
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-28 23:28:04 +0300

    small fixes & started page for creating booking

--- frontend/src/user/UserPages.jsx
+++ frontend/src/user/UserPages.jsx
@@ -1,9 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../auth/AuthContext";
 import LoginPage from "../auth/LoginPage";
-import { useNavigate } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import "../App.css"
 import "../styles/UserPages.css"
+import ErrorNotFound from "../ErrorNotFound";
 
 // todo: do update user form
 export function PersonalAccount() {
@@ -29,7 +30,7 @@ export function PersonalAccount() {
             <span className={'text-path'} onClick={() => navigate('..')}>Главная</span> 
             <span className={'text-path'}>/</span> 
             <span className={'text-path'} onClick={() => navigate('/personal-account')}>Личный кабинет</span>
-            <div id="modal-personal-account">
+            <div id="big-modal">
                 <h1>Личный кабинет</h1>
                 <div className="field-personal-account">
                     <h2>Имя</h2>{user.first_name}
@@ -49,7 +50,7 @@ export function PersonalAccount() {
     );
 }
 
-export function FreeBookings() {
+export function AllRooms() {
     const user = useContext(AuthContext).user;
     const logout = useContext(AuthContext).logout;
     const navigate = useNavigate();
@@ -58,6 +59,19 @@ export function FreeBookings() {
     const [error, setError] = useState(null);
 
     useEffect(() => {
+        async function getAllRooms() {
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/rooms");
+                setIsLoading(false);
+                if (!response.ok) {
+                    throw new Error("К сожалению, нет свободных аудиторий на ближайшее время");
+                }
+                setRooms(await response.json());
+            } catch (err) {
+                setError("К сожалению, нет свободных аудиторий на ближайшее время");
+                setIsLoading(false);
+            }          
+        }
         getAllRooms(setIsLoading, setRooms, setError);
     }, []);
 
@@ -71,40 +85,91 @@ export function FreeBookings() {
         <div id="main-container">
             <span className={'text-path'} onClick={() => navigate('..')}>Главная</span> 
             <span className={'text-path'}>/</span> 
-            <span className={'text-path'} onClick={() => navigate('/free-bookings')}>Свободные аудитории</span>
+            <span className={'text-path'} onClick={() => navigate('/rooms')}>Свободные аудитории</span>
 
             {error && <div className={'modal error'} style={{right: '0', left: '1px'}}>Ошибка<br/>{error}</div>}
-            {isLoading && <img src={'/3-dots-loader.svg'} alt={'Загрузка'} className="dots-loader"/>}
+            {isLoading && <img src={'/3-dots-loader.svg'} alt={'Загрузка'} className="loader-dots"/>}
             <div id="main-cards">
                 {rooms && rooms.map((room) => (
-                    <div key={room.id} className="room-card">
+                    <div key={room.id} className="room-card" onClick={() => navigate('/rooms/'+room.id)}>
                         {room.image && <img src={room.image} alt={room.name} />}
                         {!room.image && <img src="/image_not_found.jpeg" alt={room.name}/>}
                         <h2>{room.name}</h2>
-                        <p>{room.description}</p>
                         <p>Вместимость – {room.size}</p>
                     </div>
                 ))}
             </div>   
             {rooms && rooms.length === 0 && <div className={'modal error'} style={{right: '0', left: '1px'}}>Ошибка<br/>Нет доступных комнат</div>}
-            
         </div>
         </>
     );
 }
 
-async function getAllRooms(setIsLoading, setRooms, setError) {
-    try {
-        const response = await fetch("http://localhost:8080/api/v1/rooms");
-        setIsLoading(false);
-        if (!response.ok) {
-            throw new Error("К сожалению, нет свободных аудиторий на ближайшее время");
+export function CreateBooking() {
+    const logout = useContext(AuthContext).logout;
+    const { id } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [room, setRoom] = useState(null);
+    const navigate = useNavigate();
+    const user = useContext(AuthContext).user;
+
+    useEffect(() => {
+        async function getRoomById() {
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/rooms/"+id);
+                if (!response.ok) {
+                    if (response.status === 404) {
+                        setError('Страница не найдена');
+                        return;
+                    } else {
+                        setError('Произошла серверная ошибка');
+                        return;
+                    }
+                } 
+      
+                const data = await response.json();
+                setRoom(data);
+            } catch (err) {
+                setError('Произошла серверная ошибка');
+            } finally {
+                setIsLoading(false);
+            }
         }
-        setRooms(await response.json());
-    } catch (err) {
-        setError("К сожалению, нет свободных аудиторий на ближайшее время");
-        setIsLoading(false);
-    }          
+        getRoomById();
+    }, [])
+
+    if (error === 'Страница не найдена') {
+        return <ErrorNotFound />;
+    }
+
+    if (!user) {
+        return <LoginPage />;
+    }
+
+    return ( 
+        <>
+        <button className={"form-button"} onClick={logout} style={{right: '20px', top:'20px', position: 'absolute'}}>Выйти</button>
+        <div id="main-container">
+            <span className={'text-path'} onClick={() => navigate('/')}>Главная</span> 
+            <span className={'text-path'}>/</span> 
+            <span className={'text-path'} onClick={() => navigate('/rooms')}>Свободные аудитории</span>
+            <span className={'text-path'}>/</span> 
+            {room && <span className={'text-path'} onClick={() => navigate('/rooms/'+room.id)}>{room.name}</span>}
+
+            {error && <div className={'modal error'} style={{right: '0', left: '1px'}}>Ошибка<br/>{error}</div>}
+            {isLoading && <img src={'/3-dots-loader.svg'} alt={'Загрузка'} className="loader-dots"/>}
+            <div id={'big-modal'}>
+                <div className={'image-header-container'}>
+                    {room && room.image && <img src={room.image} alt={room.name} className={'image-header'} />}
+                    {room && !room.image && <img src={'/black-and-white-stripes.jpg'} className={'image-header'} />} 
+                </div>
+                <h1 style={{fontSize: '40px', marginTop: '40px'}}>Аудитория {room.name}</h1>
+                <p>{room.description}</p>
+            </div>  
+        </div>
+        </>
+    );
 }
 
 export function MyBookings() {
