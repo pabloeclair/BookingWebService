32d41b60420c5330b7f9d5acbbcfae6216e1c256
p.ergle <p.ergle@edu.centraluniversity.ru>
2025-06-28 19:31:56 +0300

    added findByRoomId

--- java/src/main/java/centraluniversity/app/booking/repositories/BookingRepository.java
+++ java/src/main/java/centraluniversity/app/booking/repositories/BookingRepository.java
@@ -12,4 +12,5 @@ public interface BookingRepository extends JpaRepository<BookingDbDto, Integer> 
     void deleteByRoomId(Integer roomId);
     void deleteByUserId(Integer userId);
     List<BookingDbDto> findByRoomIdAndBookingDateOrderByBookingStart(Integer roomId, LocalDate bookingDate);
+    List<BookingDbDto> findByRoomId(Integer roomId);
 }
